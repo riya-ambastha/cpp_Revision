11a3f566b47b89f6afce307bfe39a88a4ffbb84c
Riya Ambastha <ambasthariya58@gmail.com>
2024-10-30 20:35:08 +0530

    problem23.cpp

--- problem23.cpp
+++ problem23.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int jump(vector<int>& nums) {
+    int n = nums.size();
+    int jumps = 0, currentEnd = 0, farthest = 0;
+
+    for (int i = 0; i < n - 1; ++i) {
+        farthest = max(farthest, i + nums[i]);
+        
+        if (i == currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+        
+        if (currentEnd >= n - 1) {
+            break;
+        }
+    }
+
+    return jumps;
+}
+
+int main() {
+    vector<int> nums1 = {2, 3, 1, 1, 4};
+    vector<int> nums2 = {2, 3, 0, 1, 4};
+
+    cout << "Minimum jumps for nums1: " << jump(nums1) << endl;  // Output: 2
+    cout << "Minimum jumps for nums2: " << jump(nums2) << endl;  // Output: 2
+
+    return 0;
+}
