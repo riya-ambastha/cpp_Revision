708726f4e2ce618061b2fa6ce166e448b3c6ec8b
Riya Ambastha <ambasthariya58@gmail.com>
2024-11-25 23:33:42 +0530

    Problem37.cpp

---
+++ problem37.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+        
+        while (left < right) {
+            // Calculate the current area
+            int h = min(height[left], height[right]);
+            int width = right - left;
+            maxArea = max(maxArea, h * width);
+            
+            // Move the pointer with the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+};
